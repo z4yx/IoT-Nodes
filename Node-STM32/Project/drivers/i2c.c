7c96ae186b8fa83a319c6339c564ed4ee44be7ed
zhang <zz593141477@gmail.com>
2015-02-09 22:41:32 +0800

    add I2C transfer function

--- Node-STM32/Project/drivers/i2c.c
+++ Node-STM32/Project/drivers/i2c.c
@@ -8,20 +8,53 @@ void I2C_Lib_WaitForRWDone(void)
     { }
 }
 
+void I2C_Lib_MasterRW(I2C_Lib_Op_Type op,uint8_t slave_addr,uint16_t length,uint8_t* data)
+{
+    static CPAL_TransferTypeDef CPAL_Transfer; //Must be static because I2C transfer is async
+
+    I2C_Lib_WaitForRWDone();
+    
+    CPAL_Transfer.wNumData = length;
+    CPAL_Transfer.pbBuffer = data;
+    CPAL_Transfer.wAddr1 = (uint32_t)slave_addr;
+    // CPAL_Transfer.wAddr2 = (uint32_t)reg_addr;
+
+    I2C_HOST_DEV.wCPAL_Options = CPAL_OPT_NO_MEM_ADDR;
+
+    if(op == I2C_OP_Write){
+        I2C_HOST_DEV.pCPAL_TransferTx = &CPAL_Transfer;
+        if(CPAL_I2C_Write(&I2C_HOST_DEV) != CPAL_PASS){
+            ERR_MSG("CPAL_I2C_Write Failed");
+        }
+    }
+    else if(op == I2C_OP_Read){
+        I2C_HOST_DEV.pCPAL_TransferRx = &CPAL_Transfer;
+        if(CPAL_I2C_Read(&I2C_HOST_DEV) != CPAL_PASS){
+            ERR_MSG("CPAL_I2C_Read Failed");
+        }
+    }
+
+}
+
 void I2C_Lib_Init()
 {
     static bool initialized = false;
     if(initialized)
         return;
-    initialized = true;
 
     CPAL_I2C_StructInit(&I2C_HOST_DEV);
 
     // I2C_HOST_DEV.CPAL_Dev is already initialized in definition
-    I2C_HOST_DEV.pCPAL_I2C_Struct->I2C_ClockSpeed = 350000;
-    I2C_HOST_DEV.pCPAL_I2C_Struct->I2C_OwnAddress1 = 0xAA;
+    I2C_HOST_DEV.CPAL_ProgModel = CPAL_PROGMODEL_INTERRUPT;
+    // I2C_HOST_DEV.pCPAL_I2C_Struct->I2C_ClockSpeed = 350000;
+    // I2C_HOST_DEV.pCPAL_I2C_Struct->I2C_OwnAddress1 = 0xAA;
 
-    CPAL_I2C_Init(&I2C_HOST_DEV);
+    uint32_t ret = CPAL_I2C_Init(&I2C_HOST_DEV);
+    if(ret != CPAL_PASS){
+        ERR_MSG("CPAL_I2C_Init Failed");
+        return;
+    }
+    initialized = true;
 }
 
 void CPAL_I2C_ERR_UserCallback(CPAL_DevTypeDef pDevInstance, uint32_t Device_Error) 
