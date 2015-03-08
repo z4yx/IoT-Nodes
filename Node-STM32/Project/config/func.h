5eec09ce93969227fa2b3e35ef3e578732fcd748
zhang <zz593141477@gmail.com>
2015-03-08 16:00:22 +0800

    change MQTT server IP

--- Node-STM32/Project/config/func.h
+++ Node-STM32/Project/config/func.h
@@ -2,10 +2,10 @@
 #define FUNC_H__
 
 
-#define ESP_DBG_CDC
+// #define ESP_DBG_CDC
 // #define ESP_FLASH_FIRMWARE
 
-#define MQTT_BROKER_IP   "192.168.1.30"
+#define MQTT_BROKER_IP   "192.168.1.2"
 #define MQTT_BROKER_PORT 1883
 
 #endif
