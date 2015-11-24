f2b8055a7b954a29fe43491d80536b4bcef829cc
zhang <zz593141477@gmail.com>
2015-11-24 23:59:37 +0800

    add comments

--- Node-STM32/Project/config/board.h
+++ Node-STM32/Project/config/board.h
@@ -2,42 +2,42 @@
 #define BOARD_H__
 
 //===select a board here===
-#define BOARD_RFID
+#define BOARD_RELAY
 //=========================
 
-#if defined(BOARD_LIGHT_SENSOR)
+#if defined(BOARD_LIGHT_SENSOR)  //选择光线传感器
 
 #define ENABLE_BH1750
 
-#elif defined(BOARD_TEMP_HUMI)
+#elif defined(BOARD_TEMP_HUMI)  //选择湿度传感器
 
 #define ENABLE_DHT11
 
-#elif defined(BOARD_PRESSURE)
+#elif defined(BOARD_PRESSURE)  //选择气压传感器
 
 #define ENABLE_BMP180
 
-#elif defined(BOARD_RELAY)
+#elif defined(BOARD_RELAY)  //选择继电器
 
 #define ENABLE_SWITCH
 
-#elif defined(BOARD_PYROELECTRICITY)
+#elif defined(BOARD_PYROELECTRICITY)  //选择热释电传感器
 
 #define ENABLE_SR501
 
-#elif defined(BOARD_GAS)
+#elif defined(BOARD_GAS)  //选择可燃气体传感器
 
 #define ENABLE_MQ2
 
-#elif defined(BOARD_IR)
+#elif defined(BOARD_IR)  //选择红外遥控
 
 #define ENABLE_IR
 
-#elif defined(BOARD_RFID)
+#elif defined(BOARD_RFID)  //选择射频卡读卡器
 
 #define ENABLE_PN532
 
-#elif defined(BOARD_CUSTOM)
+#elif defined(BOARD_CUSTOM)  //自定义传感器、执行器
 
 #define ENABLE_DHT11
 #define ENABLE_BMP180
