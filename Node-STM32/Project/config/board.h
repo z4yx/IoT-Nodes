6c422b7375bddd11af7bd1c574c2d8bc857c0f9c
zhang <zz593141477@gmail.com>
2016-02-23 10:23:32 +0800

    change board config header

--- Node-STM32/Project/config/board.h
+++ Node-STM32/Project/config/board.h
@@ -1,57 +1,37 @@
 #ifndef BOARD_H__
 #define BOARD_H__
 
-//===修改这个定义来选择外设===
-#define BOARD_RELAY
-//=========================
 
-#if defined(BOARD_LIGHT_SENSOR)  //选择光线传感器
-
+//选择光线传感器
 #define ENABLE_BH1750
 
-#elif defined(BOARD_TEMP_HUMI)  //选择湿度传感器
+//选择湿度传感器
+// #define ENABLE_DHT11
 
-#define ENABLE_DHT11
+//选择气压传感器
+// #define ENABLE_BMP180
 
-#elif defined(BOARD_PRESSURE)  //选择气压传感器
+//选择继电器
+// #define ENABLE_SWITCH
 
-#define ENABLE_BMP180
+//选择PWM输出，舵机、功率控制等
+// #define ENABLE_PWM
 
-#elif defined(BOARD_RELAY)  //选择继电器
+//选择热释电传感器
+// #define ENABLE_SR501
 
-#define ENABLE_SWITCH
+//选择可燃气体传感器
+// #define ENABLE_MQ2
 
-#elif defined(BOARD_PWM)  //选择PWM输出，舵机、功率控制等
+//选择红外遥控
+// #define ENABLE_IR
 
-#define ENABLE_PWM
+//选择射频卡读卡器
+// #define ENABLE_PN532
 
-#elif defined(BOARD_PYROELECTRICITY)  //选择热释电传感器
+//选择电功率表
+// #define ENABLE_LT211
 
-#define ENABLE_SR501
 
-#elif defined(BOARD_GAS)  //选择可燃气体传感器
-
-#define ENABLE_MQ2
-
-#elif defined(BOARD_IR)  //选择红外遥控
-
-#define ENABLE_IR
-
-#elif defined(BOARD_RFID)  //选择射频卡读卡器
-
-#define ENABLE_PN532
-
-#elif defined(BOARD_POWER_METER)  //选择电功率表
-
-#define ENABLE_LT211
-
-#elif defined(BOARD_CUSTOM)  //自定义传感器、执行器
-
-#define ENABLE_DHT11
-#define ENABLE_BMP180
-
-#else
-#error Must choose one board
-#endif
 
 #endif
